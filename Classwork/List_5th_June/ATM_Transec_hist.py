c3b159d1f10ab65885d8da18779b65d7ce36bff0
Istikamat Ali <a2024cseaiml11559@imsec.ac.in>
2026-06-05 14:58:16 +0530

    ATM Transaction History

---
+++ Classwork/List_5th_June/ATM_Transec_hist.py
@@ -0,0 +1,10 @@
+'''Problem Statement
+A customer's transactions are stored as:
+transactions = [5000, -2000, 3000, -1000, -500, 7000]
+Positive values represent deposits and negative values represent withdrawals.
+Write a program to:
+1. Calculate the current balance. 
+2. Count total deposits and withdrawals. 
+3. Find the largest deposit and largest withdrawal. 
+4. Create separate lists for deposits and withdrawals.'''
+#
