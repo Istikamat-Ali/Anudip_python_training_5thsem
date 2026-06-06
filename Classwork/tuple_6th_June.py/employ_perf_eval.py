9bf5f651bc1b4f1a88dd74029a8cc29dc374f6ef
Istikamat Ali <a2024cseaiml11559@imsec.ac.in>
2026-06-06 10:49:32 +0530

    Employee Performance Analyzer

--- Classwork/tuple_6th_June.py/employ_perf_eval.py
+++ Classwork/tuple_6th_June.py/employ_perf_eval.py
@@ -21,7 +21,24 @@ Write a Python program to:
 o 90 and above → Excellent 
 o 75 to 89 → Good 
 o 60 to 74 → Average 
-o Below 60 → Needs Improvement'''
+o Below 60 → Needs Improvement
+
+Sample Output
+Employees Scoring 80 or Above:
+E101 Anuj 92
+E104 Neha 88
+Employees Needing Improvement: 2
+Highest Performer:
+E101 Anuj 92
+High Performers:
+['Anuj', 'Rahul', 'Neha']
+Performance Categories:
+Anuj -> Excellent
+Rahul -> Good
+Priya -> Needs Improvement
+Neha -> Good
+Amit -> Needs Improvement'''
+print("------------Employee Performance Analyzer---------")
 #given tuple
 employees = (
  ("E101", "Anuj", 92),
@@ -31,13 +48,11 @@ employees = (
  ("E105", "Amit", 45)
 )
 #Task 1: To display details of employees scoring 80 or above.
-print("----- Employee Records -----")
+print("Employees Scoring 80 or Above:")
 for record in employees:
   if(record[2] >= 80):#if performance score is 80 or above
-    print("Employee ID : ",record[0])
-    print("Name : ",record[1])
-    print("Performance Score : ",record[2])
-    print("---------------------------------")
+    print(record[0],record[1],record[2])
+print("---------------------------------")
 #-----------------------------------------
 #Task 2: To count the number of employees who need improvement
 count = 0
@@ -48,44 +63,36 @@ print("Number of employees need improvement : ",count)
 print("------------------------------") 
 #-----------------------------------------
 #Task 3: Find the employee with the highest score
+print("Highest Performer:")
 #initializing highest score record
 highest_score = 0
 for record in employees:
   if(record[2] > highest_score):#if performance score is greater than highest score
     highest_score = record[2]#update highest score
     highest_score_record = record#update highest score record
-print("Employee with highest score : ")
-print("Employee ID : ",highest_score_record[0])
-print("Name : ",highest_score_record[1])
-print("Performance Score : ",highest_score_record[2])
+
+print(highest_score_record[0],highest_score_record[1],highest_score_record[2])
 print("---------------------------------")
 #-----------------------------------------
 #Task 4: Create a list containing the names of all employees scoring above 75.
+print("High Performers : ")
 #taking an empty variable for names
 names = []
 for record in employees:
   if(record[2] > 75):#if performance score is greater than 75
     names.append(record[1])#add name to list
-print("Names of employees scoring above 75 : ",names)
+print(names)
 print("------------------------------")
-print("----------------------------------")
 #-----------------------------------------
+print("Performance Categories : ")
 #Task 5: Display the performance category for each employee
 for record in employees:
   if(record[2] >= 90):#if performance score is 90 or above
-    print("Employee ID : ",record[0])
-    print("Name : ",record[1])
-    print("Performance Category : Excellent")
+    print(record[0],end=" -> Excellent\n")
   elif(record[2] >= 75 and record[2] < 90):#if performance score is between 75 and 90
-    print("Employee ID : ",record[0])
-    print("Name : ",record[1])
-    print("Performance Category : Good")
+    print(record[0],end=" -> Good\n")
   elif(record[2] >= 60 and record[2] < 75):#if performance score is between 60 and 75
-    print("Employee ID : ",record[0])
-    print("Name : ",record[1])
-    print("Performance Category : Average")
+    print(record[0],end=" -> Average\n")
   elif(record[2] < 60):#if performance score is less than 60
-    print("Employee ID : ",record[0])
-    print("Name : ",record[1])
-    print("Performance Category : Needs Improvement")
-  print("---------------------------------")
+    print(record[0],end=" -> Needs Improvement\n")
+print("---------------------------------")
