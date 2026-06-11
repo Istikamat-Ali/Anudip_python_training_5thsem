f7c028a005deea544898732b28d41ca2289f19ed
Istikamat Ali <a2024cseaiml11559@imsec.ac.in>
2026-06-11 11:42:20 +0530

    HOspital patiennt Management System

--- Classwork/python_coding_challenge_1/hospital_pateint_record_mangmt_sys.py
+++ Classwork/python_coding_challenge_1/hospital_pateint_record_mangmt_sys.py
@@ -32,39 +32,49 @@ P104,Neha,Critical
 Critical Patient Report Generated Successfully.'''
 #dispay all patient records
 with open("patients.txt","r") as file:
-    for file in file.strip().split("    \n"):
-        print(file)
+    for line in file:
+        print(line.strip())
+        #p_id,p_name,p_status=line.strip().split(",")#splitting line by comma in a list
+        #print(p_id,p_name,p_status)
 #display critical patients
+print("Critical Patients:")
 with open("patients.txt","r") as file:
-    for file in file.strip().split("\n"):
+    for line in file:
+        p_id,p_name,p_status=line.strip().split(",")#splitting line by comma in a list
         if "Critical" in file:
-            print(file)
+            print(p_name)
 #count patients under each status
+print("Patient Count:")
 with open("patients.txt","r") as file:
     normal_count=0
     stable_count=0
     critical_count=0
-    for file in file.strip().split("\n"):
-        if "Normal" in file:
+    for f in file:
+        p_id,p_name,p_status=f.strip().split(",")
+        if "Normal" in f:
             normal_count+=1
-        elif "Stable" in file:
+        elif "Stable" in f:
             stable_count+=1
-        elif "Critical" in file:
+        elif "Critical" in f:
             critical_count+=1
     print("Normal:",normal_count)
     print("Stable:",stable_count)
     print("Critical:",critical_count)
 #search patient details using Patient ID
+patient_id=input("Enter Patient ID to search:")
 with open("patients.txt","r") as file:
-    for file in file.strip().split("\n"):
-        if "P104" in file:
+    for line in file:
+        p_id,p_name,p_status=line.strip().split(",")
+        if patient_id==p_id:
             print("Patient Found:")
-            print(file)
+            print(line.strip())
             break
-        
+        else:
+            print("Patient not found.")
 #save critical patient records to critical_patients.txt
-with open("critical_patients.txt","w") as file:
-    for file in file.strip().split("\n"):
-        if "Critical" in file:
-            file.write(file)
+with open("patients.txt","r") as file:
+    for line in file:
+        if "Critical" in line:
+            with open("critical_patients.txt","a") as f:
+                f.write(line)
 print("Critical Patient Report Generated Successfully.")
